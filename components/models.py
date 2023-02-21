c57c62af7c62609a694446a2590ed892d8309f30
Avinash Mishra <avinashmishra1904@gmail.com>
2023-02-22 02:19:05 +0530

    CS

--- components/models.py
+++ components/models.py
@@ -123,7 +123,7 @@ class Customer(UUIDModel):
     name = models.CharField(max_length=50)
     email = models.EmailField()
     phone = models.CharField(max_length=50)
-    location = models.ManyToManyField(Location, related_name="locations")
+    location = models.ManyToManyField(Location, related_name="locations", blank=True)
 
     def __str__(self):
         return self.name
