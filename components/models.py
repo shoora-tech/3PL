e90651dd2adcb84f7ab9a832d95b0c3bff3ea9d4
abhishek <abhishekdwivediofficial@gmail.com>
2023-02-22 19:47:11 +0530

    changes in transporter and other fields added

--- components/models.py
+++ components/models.py
@@ -59,6 +59,14 @@ class Transporter(UUIDModel):
     poc_email = models.EmailField()
     poc_name = models.CharField(max_length=50)
     poc_phone = models.CharField(max_length=50)
+    name_in_bank_TZS_account = models.CharField(max_length=50,blank=True, null=True)
+    bank_name_TZS_account = models.CharField(max_length=50,blank=True, null=True)
+    swift_code_TZS_account = models.CharField(max_length=20,blank=True, null=True)
+    account_number_TZS_account = models.IntegerField( blank=True, null=True)
+    name_in_bank_USD_account = models.CharField(max_length=50,blank=True, null=True)
+    bank_name_USD_account = models.CharField(max_length=50,blank=True, null=True)
+    swift_code_USD_account = models.CharField(max_length=20,blank=True, null=True)
+    account_number_USD_account = models.IntegerField( blank=True, null=True)
 
     def __str__(self):
         return self.name
@@ -70,6 +78,8 @@ class TransporterOrganizationFuel(UUIDModel):
     fuel_discount = models.FloatField(default=0)
     station = models.ForeignKey(Station, on_delete=models.CASCADE)
 
+    class Meta:
+        verbose_name_plural = "Discount Master"
 
 class Vehicle(UUIDModel):
     vehicle_number = models.CharField(max_length=10)
